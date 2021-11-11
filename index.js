7e197780f0d8db17f6e9664b4354dcdeb0464e04
sakin-hossain <info.sakinh@gmail.com>
2021-11-12 01:57:13 +0600

    car individual added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const ObjectId = require('mongodb').ObjectId;
 
 require('dotenv').config();
 
@@ -22,6 +23,7 @@ async function run(){
         await client.connect();
         const database = client.db("carmaxDb");
         const carCollection = database.collection("cars");
+        const ordersCollection = database.collection("orders");
 
       app.post('/addCar', async (req,res)=>{
         const result = await carCollection.insertOne(req.body);
@@ -32,6 +34,27 @@ async function run(){
         const result = await carCollection.find({}).toArray();
         res.json(result);
       });
+      app.get('/addCar/:id', async (req,res)=>{
+        const query = { _id: ObjectId(req.params.id) }
+        const car = await carCollection.findOne(query);
+        res.send(car);
+    });
+      app.get('/myOrders', (req,res)=>{
+        const email = req.query.email;
+        const query = {email: email}
+        const cursor = await ordersCollection.find({query});
+        const orders = await cursor.toArray();
+        res.json(orders);
+      });
+      app.post('/myOrders', async(req,res)=>{
+        const result = await ordersCollection.insertOne(req.body);
+        res.send(result);
+    });
+    app.delete('/myOrders/:id', async(req,res)=>{
+      const query = {_id: ObjectId(req.params.id)};
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+  });
     }
     finally{
         // await
